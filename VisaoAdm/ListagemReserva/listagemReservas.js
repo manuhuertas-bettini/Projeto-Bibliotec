dddf6a9501b86f61e726b6137bfc7d92ceb511e8
MANUELLA HUERTAS BETTINI <50630939810@info.local>
2026-09-16 17:25:57 -0300

    :sparkles: feat: Adiciona listagemReservas.js.

---
+++ VisaoAdm/ListagemReserva/listagemReservas.js
@@ -0,0 +1,15 @@
+const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+const statusRetirada = document.querySelectorAll(".status");
+checkboxes.forEach((caixinha, indice, lista)=>{
+     caixinha.addEventListener("change", () => {
+        if(caixinha.checked == true){
+            statusRetirada[indice].classList.remove("aguardando")
+            statusRetirada[indice].classList.add("retirado")
+            statusRetirada[indice].textContent = "Retirado"
+        }else{
+            statusRetirada[indice].classList.remove("retirado")
+            statusRetirada[indice].classList.add("aguardando")
+            statusRetirada[indice].textContent = "Aguardando retirada"
+        }
+    })
+})
