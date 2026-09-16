652683394beebce98cedca21fba41c5c780812cf
MANUELLA HUERTAS BETTINI <50630939810@info.local>
2026-09-16 16:56:22 -0300

    :sparkles: feat: Organiza estrutura do projeto.

---
+++ home/home.js
@@ -0,0 +1,10 @@
+const menu = document.getElementById("menu");
+const navMenu = document.querySelector(".navegacao");
+menu.addEventListener("click", function() {
+    if(navMenu.className == "navegacao") {
+        navMenu.className = "navegacao ativo";
+    }
+    else {
+        navMenu.className = "navegacao";
+    }
+})
